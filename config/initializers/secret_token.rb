42256622ec1d4b2cf4c221b34ac59bc6d901c054
Ryo Suzuki <ryooopan@googlemail.com>
2013-05-20 11:56:07 +0900

    create rails-twitter-app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTwitterApp::Application.config.secret_token = '73cd50d0d63255132d253adc0986ebd01ba4909a044ca46898012f45360001db9e4219bf269f52d2ec9b6ff2b83b1aff2d557a98b9970b438444beb954ed03e4'
