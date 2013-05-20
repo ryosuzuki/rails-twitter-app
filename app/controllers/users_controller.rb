be57f78e03fe8e8930a11898236f88607937b404
Ryo Suzuki <ryooopan@googlemail.com>
2013-05-20 12:56:49 +0900

    get twitter timeline with twitter gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
 
   def index
-
+    @user = User.all
   end
 
   def show
-    
+    @user = User.find(params[:id])
+    Twitter.configure do |config|
+      config.consumer_key = ENV['TWITTER_KEY']
+      config.consumer_secret = ENV['TWITTER_SECRET']
+    end
+    @client = Twitter::Client.new(:oauth_token => @user.token, :oauth_token_secret => @user.secret)
+    @tweets = @client.user_timeline(@user.uid)
   end
 
 end
